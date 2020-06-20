d5d95656288f24e26a91c74ebbc14bcca8c155aa
Leo Ayala <leo_a1016@yahoo.com>
2020-06-20 12:48:44 -0400

    added a fight loop with a 'for' loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,23 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android" , "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + " index");
+}
+
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -60,4 +72,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
   }
 }
-  fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  };
